321473d59ad08caa177497b4b768322ae4e15948
Gary <glswim@gmail.com>
2024-02-21 00:57:20 +0800

    Added some style

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,6 +1,9 @@
+import cvMaker from "../images/cv-maker.png";
+
 export default class Work{
     static load = () =>{
         const container = document.createElement("div");
+        container.classList.add("section-container");
         container.setAttribute("id", "work");
 
     
@@ -10,7 +13,8 @@ export default class Work{
     }
 
     static title = () =>{
-        const title = document.createElement("h1");
+        const title = document.createElement("h2");
+        title.classList.add("section-title");
         title.textContent = "Work";
 
         return title;
@@ -18,22 +22,16 @@ export default class Work{
 
     static projects = () =>{
         const projects = [
-            {name: "Project 1", 
+            {name: "Tic Tac Toe", 
             description: "A Tic Tac Toe project implemented with HTML, CSS, and JavaScript. Players take turns marking squares in a 3x3 grid to get three in a row horizontally, vertically, or diagonally. CSS styles the game layout, while JavaScript handles game logic such as checking for wins and switching between player turns.", 
             image: ""},
-            {name: "Project 2", 
+            {name: "Weather App", 
             description: "A Tic Tac Toe project implemented with HTML, CSS, and JavaScript. Players take turns marking squares in a 3x3 grid to get three in a row horizontally, vertically, or diagonally. CSS styles the game layout, while JavaScript handles game logic such as checking for wins and switching between player turns.", 
             image: ""},
-            {name: "Project 3", 
+            {name: "CV Maker", 
             description: "A Tic Tac Toe project implemented with HTML, CSS, and JavaScript. Players take turns marking squares in a 3x3 grid to get three in a row horizontally, vertically, or diagonally. CSS styles the game layout, while JavaScript handles game logic such as checking for wins and switching between player turns.", 
             image: ""},
-            {name: "Project 4", 
-            description: "A Tic Tac Toe project implemented with HTML, CSS, and JavaScript. Players take turns marking squares in a 3x3 grid to get three in a row horizontally, vertically, or diagonally. CSS styles the game layout, while JavaScript handles game logic such as checking for wins and switching between player turns.", 
-            image: ""},
-            {name: "Project 5", 
-            description: "A Tic Tac Toe project implemented with HTML, CSS, and JavaScript. Players take turns marking squares in a 3x3 grid to get three in a row horizontally, vertically, or diagonally. CSS styles the game layout, while JavaScript handles game logic such as checking for wins and switching between player turns.", 
-            image: ""},
-            {name: "Project 6", 
+            {name: "Restaurant", 
             description: "A Tic Tac Toe project implemented with HTML, CSS, and JavaScript. Players take turns marking squares in a 3x3 grid to get three in a row horizontally, vertically, or diagonally. CSS styles the game layout, while JavaScript handles game logic such as checking for wins and switching between player turns.", 
             image: ""},
         ]
@@ -41,17 +39,29 @@ export default class Work{
         const createElement = document.createElement("div");
         createElement.classList.add("project-container");
         projects.forEach((item) =>{
-            const createContainer = document.createElement("div");
-            createContainer.innerHTML = `
-                <h2 class="project-title">${item.name}</h2>
-                <div class="project-left">image</div>
+
+            const container = document.createElement("div");
+
+            const title = document.createElement("h2");
+            title.classList.add("title-name")
+            title.textContent = item.name;
+
+            const projectCards = document.createElement("div");
+            projectCards.classList.add("project-cards")
+            projectCards.innerHTML = `
+                <div class="project-left">
+                    <img class="project-image" src="${cvMaker}" alt="An image of ${item.name}" />
+                </div>
                 <div class="project-right">
                     <div class="text-container">
                         <p class="project-description">${item.description}</p>
                     </div>
                 </div>`;
 
-                createElement.appendChild(createContainer);
+                container.appendChild(title);
+                container.appendChild(projectCards);
+
+                createElement.appendChild(container);
         });
 
         return createElement;
