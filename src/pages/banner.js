75fea5c542c97c0cd23900f3fcb742699288f90c
Gary <glswim@gmail.com>
2024-02-28 10:21:51 +0800

    Added typewriter function to title and worked on button

--- src/pages/banner.js
+++ src/pages/banner.js
@@ -3,24 +3,59 @@ export default class Banner{
         const container = document.createElement("div");
         container.classList.add("banner-container");
 
-        container.innerHTML = `
-            <h1>Gary Lei</h1>
-            <p>A full-stack developer</p>
-        `;
+        container.appendChild(this.title());
+        container.appendChild(this.subText());
+        container.appendChild(this.bannerHandlerBtn());
 
-        const button = document.createElement("button");
-        button.textContent = "Yo";
-        button.addEventListener(("click"), () =>{console.log("Hello")});
-        // const title = document.createElement("h1");
-        // title.textContent = "Gary Lei";
-
-        container.appendChild(button);
-        // const subTitle = document.createElement("p");
-        // subTitle.textContent = "A full-stack developer";
-        
-        
-        // container.appendChild(title);
-        // container.appendChild(subTitle);
         return container;
     }
+
+    static title = () =>{
+        const text = document.createElement("h1");
+        text.classList.add("banner-title")
+        text.textContent = "Gary Lei";
+        return text;
+    }
+    static subText = () =>{
+        const subtitle = "A web developer and an ESL teacher"
+        const text = document.createElement("p");
+        text.classList.add("typeWriter", "subtitle");
+
+        let index = 0;
+
+        const typeWriter = () =>{
+            if(index < subtitle.length){
+                text.innerHTML += subtitle.charAt(index);
+                index++;
+                setTimeout(typeWriter, 200)    
+            } else {
+                setTimeout(resetIndex, 3000);
+            }
+        }
+
+        const resetIndex = () =>{
+            text.innerHTML = "" //clear the title
+            index = 0;
+            typeWriter(); //Start typewrite effect again
+        }
+        typeWriter();
+
+        return text;
+    }
+
+    static bannerHandlerBtn = () =>{
+        const button = document.createElement("button");
+        button.classList.add("banner-btn-nav-about")
+        button.textContent = "Learn More";
+
+        const scrollToSection = () => {
+            const section = document.getElementById("about");
+            section.scrollIntoView({behavior: "smooth"});
+        }
+        button.addEventListener(("click"), scrollToSection);
+
+        return button;
+    }
+
+
 }
