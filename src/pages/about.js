21c2ab0b2a9bee057252450a4ae2a61a95194f75
Gary <glswim@gmail.com>
2024-02-28 10:57:30 +0800

    Fixed about section

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,4 @@
+import plotSocialMediaIcons from "../components/socialMediaIcons";
 import profilePicture from "../images/profilePicture.png";
 
 export default class About{
@@ -5,8 +6,6 @@ export default class About{
         const container = document.createElement("div");
         container.classList.add("section-container","about-background");
         container.setAttribute("id", "about");
-
-        container.appendChild(this.title());
 
         const imageDescription = document.createElement("div");
         imageDescription.classList.add("imageDescription-container");
@@ -16,20 +15,30 @@ export default class About{
         container.appendChild(imageDescription);
         return container;
     }
-    static title = () =>{
-        const title = document.createElement("h2");
-        title.classList.add("section-title");
-        title.textContent = "About me";
-
-        return title;
-    }
 
     static personalDescription = () =>{
-        const p = document.createElement("p");
-        p.classList.add("about-description");
-        p.textContent = "As an ESL English teacher, I'm committed to fostering language proficiency and cultural understanding. Outside the classroom, I'm an avid runner, finding solace in each stride, and a passionate real estate enthusiast, exploring the intricacies of property investment. Balancing professional growth with personal pursuits drives my journey.";
+        const container = document.createElement("div");
+        container.classList.add("about-description");
 
-        return p;
+        const title = () =>{
+            const title = document.createElement("h2");
+            title.textContent = "About me";
+            return title;
+        }
+
+        const description = () =>{
+            const p = document.createElement("p");
+            p.textContent = "As an ESL English teacher, I'm committed to fostering language proficiency and cultural understanding. Outside the classroom, I'm an avid runner, finding solace in each stride, and a passionate real estate enthusiast, exploring the intricacies of property investment. Balancing professional growth with personal pursuits drives my journey.";
+    
+            return p;
+        }
+
+        container.appendChild(title());
+        container.appendChild(description());
+        container.appendChild(plotSocialMediaIcons("about"));
+
+        return container;
+  
     }
 
     static loadImage = () =>{
