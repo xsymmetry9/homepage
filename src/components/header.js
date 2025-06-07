21b3c3ac8b8615579aeba2fa9c14ef0a2d3ebdde
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-06-07 23:18:53 +0800

    Fix bug

--- src/components/header.js
+++ src/components/header.js
@@ -5,6 +5,7 @@ export default class Header{
         const headerContainer = document.createElement("div");
         headerContainer.classList.add("header-container");
         
+        //Sticky on scroll
         const handleScroll = () =>{
             if(window.pageYOffset != 0){
                 element.classList.add("sticky")
@@ -15,6 +16,7 @@ export default class Header{
 
         window.addEventListener("scroll", handleScroll);
     
+        // Logo title
         const title = (logoName) =>{
             const box = document.createElement("div");
             box.className = "title-logo";
@@ -23,13 +25,18 @@ export default class Header{
             return box;
         }
 
+        // Hamburger menu button
         const menuButton = () =>{
             const button = document.createElement("button");
             button.classList.add("menu-btn");
             button.setAttribute("aria-controls", "primary-navigation");
-            button.setAttribute("aria-expanded", false);
+            const setVisibility = () => {
+                button.setAttribute("aria-hidden", window.innerWidth <= 800);
+                button.setAttribute("aria-expanded", false);
+            }
+            setVisibility();
             button.innerHTML = `
-                <svg fill="var(--button-color)" class ="hamburger" viewBox ="0 0 100 100" width ="42" height = "42">
+                <svg fill="var(--button-color)" class ="hamburger" viewBox ="0 0 100 100" width ="30" height = "30">
                     <rect class ="line top" width ="80" height="6" x="10" y="30" rx="5">
                     </rect>
                     <rect class ="line middle" width ="80" height="6" x="10" y="50" rx="5">
@@ -39,24 +46,29 @@ export default class Header{
             return button;
         }
     
+        //Navigation
+        //Bug: 
+        //When window is minimized, navigation becomes in visible which is ok.  But, when maximize the window, navigation stays hidden.
         const navigation = () => {
             const container = document.createElement("nav");
             container.className = "primary-navigation";
-            if(window.innerWidth > 880)
-            {
-                container.setAttribute("aria-hidden", false);
-            }
-            else{
-                container.setAttribute("aria-hidden", true);
-            }
-            
+            container.setAttribute("aria-hidden", false);
+            container.setAttribute("aria-expanded", true);
+
+            // const setVisibility = () =>{
+            //     container.setAttribute("aria-hidden", window.innerWidth <= 880);
+            //     container.setAttribute("aria-expanded", false);
+            // };
+            // setVisibility();
+            // window.addEventListener("resize", setVisibility); // This fixes for resize bug
+                        
             const lists = () => {
                 const unOrderedList = document.createElement("ul");
                 unOrderedList.classList.add("nav-container");
                  const tabs = [{page: "about", link: "about"}, {page: "work", link: "work"}, {page: "contact", link: "contact"}]
                  tabs.forEach((item) =>{
                     const list = document.createElement("li");
-                    list.innerHTML = `<a class="nav-links">${item.page}</a>`;
+                    list.innerHTML = `<a href="#${item.page}" class="nav-links">${item.page}</a>`;
                     unOrderedList.appendChild(list);
                  });
     
