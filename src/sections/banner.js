8f58427f87eae7bcafe37e3474475183a8008ce0
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-06-08 21:32:47 +0800

    Fix image: make image responsive

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -1,19 +1,5 @@
-const typeWriter = (title, element) =>{
+import { typeWriter } from "../components/typewriter";
 
-    for (let i = 0; i <= title.length; i++) {
-        if(i != title.length)
-        {
-            setTimeout(() => {
-                element.innerHTML = title.slice(0, i) + title.charAt(i) + `<span class="blink">|</span>`;
-            }, i * 100 + (Math.random() * 100));
-        } else {
-            setTimeout(() => {
-                element.innerHTML = title;
-            }, i * 100 + (Math.random() * 150));
-        }
-    }
-
-}
 export default class Banner{
     static load = () =>{
         const container = document.createElement("div");
