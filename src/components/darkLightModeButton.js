f51fb701b5b2d3a741b3b76aab4163654e7e4efd
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-06-14 22:53:07 +0800

    Change text content

--- src/components/darkLightModeButton.js
+++ src/components/darkLightModeButton.js
@@ -1,14 +1,35 @@
-const darkLightModeButton = () => {
+const darkLightModeButton = () => {   
     const toggleButton = document.createElement("button");
     toggleButton.setAttribute("id", "theme-toggle");
-    toggleButton.textContent = "Toggle Theme";
-
-    let isDark = false
+    console.log(document.documentElement.getAttribute("data-theme"));
+    toggleButton.textContent = "Light";
+    let isDark = false;
+    
     toggleButton.addEventListener(("click"), () => {
+
         isDark = !isDark;
         document.documentElement.setAttribute("data-theme", isDark ? "dark" : "light");
-        console.log(document.documentElement.getAttribute("data-theme"));
+        toggleButton.textContent = `${isDark ? "Dark" : "light"}`
     })
+
+    const scrollHandler = () => {
+        const scrollPosition = window.pageYOffset;
+        const windowHeight = window.innerHeight;
+        const documentHeight = document.documentElement.scrollHeight;
+  
+        // When it's scroll to the buttom, adds a class called absolute and when it's
+    // not, adds a class "sticky"
+        if(windowHeight + scrollPosition != documentHeight) {
+            toggleButton.classList.remove("absolute");
+            toggleButton.classList.add("sticky");
+        } else {
+            toggleButton.classList.remove("sticky");
+            toggleButton.classList.add("absolute");
+        }
+
+    }
+    window.addEventListener("scroll", scrollHandler);
+
 
     return toggleButton;
 }
