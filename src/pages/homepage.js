ea8da5dc4944d0a0468260867e8ab98b16223675
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-06-14 08:45:04 +0800

    Remove skill section

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -3,7 +3,6 @@ import footer from '../components/footer.js';
 import banner from "../sections/banner.js";
 import work from "../sections/work.js";
 import about from "../sections/about.js";
-import skills from "../sections/skills.js";
 import contact from "../sections/contact.js";
 import eventHandlers from "../components/eventHandlers.js";
 
@@ -24,7 +23,6 @@ export default class HomePage {
 
         content.appendChild(banner.load());
         content.appendChild(about.load());
-        content.appendChild(skills.load());
         content.appendChild(work.load());
         content.appendChild(contact.load());
 
